ca40923ef9d5ca867b91e6af351d06d1c9251eea
AaronRodrigues <aaronrod@gmail.com>
2018-06-25 08:47:50 +0100

    Adds First Three Tests for Conways Game of Life -All Tests Pass

---
+++ GameOfLife/Conway.cs
@@ -0,0 +1,30 @@
+﻿namespace TDDKata.GameOfLife
+{
+    public enum Cellstate
+    {
+        Alive,
+        Dead
+    }
+
+    public class LifeRules
+    {
+        public static Cellstate GetNewState(Cellstate currentState, int liveNeighbours)
+        {
+            if (currentState == Cellstate.Alive && liveNeighbours < 2)
+            {
+                currentState = Cellstate.Dead;
+            }
+            if (currentState == Cellstate.Alive && liveNeighbours > 3)
+            {
+                currentState = Cellstate.Dead;
+            }
+            if (currentState == Cellstate.Dead && liveNeighbours == 3)
+            {
+                currentState = Cellstate.Alive;
+            }
+            return currentState;
+        }
+    }
+
+    
+}
---
+++ TDDKata.Tests/GameOfLife/GameOfLifeTests.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using TDDKata.GameOfLife;
+
+namespace TDDKata.Tests.GameOfLife
+{
+    [TestFixture]
+    public class GameOfLifeTests
+    {
+
+        [Test]
+        public void foo()
+        {
+            Assert.AreEqual(2, 2);
+        }
+
+        [Test]
+        public void AnyLiveCellWithFewerthanTwoLiveNeighborsDiesAsIfByUnderPopulation()
+        {
+            Cellstate currentState = Cellstate.Alive;
+            int liveNeighbours = 1;
+            Cellstate result = LifeRules.GetNewState(currentState, liveNeighbours);
+            Assert.AreEqual(Cellstate.Dead, result);
+        }
+
+
+        [Test]
+        public void LiveCell_TwoOrThreeLiveNeighbours_LivestoNextGeneration()
+        {
+            Cellstate currentState = Cellstate.Alive;
+            int liveNeghbours = 3;
+            Cellstate result = LifeRules.GetNewState(currentState, liveNeghbours);
+            Assert.AreEqual(Cellstate.Alive, result);
+        }
+
+        [Test]
+        public void LiveCell_MoreThanThreeLiveNeighbours_DiesByOverpopulation()
+        {
+            Cellstate currentState = Cellstate.Alive;
+            int liveNeghbours = 4;
+            Cellstate result = LifeRules.GetNewState(currentState, liveNeghbours);
+            Assert.AreEqual(Cellstate.Dead, result);
+        }
+
+        [Test]
+        public void DeadCell_ExactlyThreeLiveNeighbours_BecomesLiveByReproduction()
+        {
+            Cellstate currentState = Cellstate.Dead;
+            int liveNeghbours = 3;
+            Cellstate result = LifeRules.GetNewState(currentState, liveNeghbours);
+            Assert.AreEqual(Cellstate.Alive, result);
+        }
+        //   Any live cell with fewer than two live neighbors dies, as if by under population.
+        //   Any live cell with two or three live neighbors lives on to the next generation.
+        //   Any live cell with more than three live neighbors dies, as if by overpopulation.
+        //   Any dead cell with exactly three live neighbors becomes a live cell, as if by reproduction.
+    }
+}
